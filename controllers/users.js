29fd6a6e7b4bb952e5bf8284d2d98b98dee52e36
ResidentEvilGod <resmaster19@gmail.com>
2025-12-15 10:37:25 -0600

    Project 12 video 8

--- controllers/users.js
+++ controllers/users.js
@@ -31,7 +31,8 @@ const getUser = (req, res) => {
       console.error(err);
       if (err.name === "DocumentNotFoundError") {
         return res.status(404).send({ message: err.message });
-      } else return res.status(400).send({ message: err.message });
+      } else if (err.name === "CastError")
+        return res.status(400).send({ message: err.message });
     });
   return res.status(500).send({ message: err.message });
 };
