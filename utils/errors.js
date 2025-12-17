ff0e112b9476270ec0dbdba2c085dcbbc576fafc
ResidentEvilGod <resmaster19@gmail.com>
2025-12-17 15:33:59 -0600

    Project 12 update 1.1

---
+++ utils/errors.js
@@ -0,0 +1,12 @@
+const BAD_REQUEST = 400;
+const NOT_FOUND = 404;
+const DEFAULT_ERROR = 500;
+
+const DEFAULT_ERROR_MESSAGE = "An error has occurred on the server.";
+
+module.exports = {
+  BAD_REQUEST,
+  NOT_FOUND,
+  DEFAULT_ERROR,
+  DEFAULT_ERROR_MESSAGE,
+};
